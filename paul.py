50a49f99b36db8b47098ca10f98a5e20c87db866
jodathecoda <jodathecoda@yahoo.com>
2016-03-04 08:09:44 +0200

    paul added result and money

--- paul.py
+++ paul.py
@@ -1,32 +1,7 @@
 #!/usr/bin/python3
 import datetime
 
-#from italy.functions.calculations import *
-
 debug=0
-
-#homeTeam = ItalyTeam()
-#awayTeam = ItalyTeam()
-
-#homeData=0
-#awayData=0
-
-#country=""
-
-#nationalTeam = input("Enter first team name: ")
-#country = checkNames(nationalTeam)
-
-
-#homeTeam.name = setName(nationalTeam)
-#awayTeam.name = input("Enter second team name: ")
-#if country == "Italy":
-#    awayTeam.name = setName(awayTeam.name)
-
-#if country == "Italy":
-#    homeData = findName(homeTeam.name)
-#    awayData = findName(awayTeam.name)
-#    populateData(homeTeam)
-#    populateData(awayTeam)
 
 first_team_name = input("Enter first team name: ")
 second_team_name = input("Enter second team name: ")
@@ -92,65 +67,7 @@ if debug == 1 :
     print(str(last5games_percent_home))
     print(str(last5games_percent_draw))
     print(str(last5games_percent_away))
-
-
-# Last 5 meetings
-
-#if homeData ==1 and awayData ==1:
-#    first_team_5meetings_wins = get5meetingswins(homeTeam.name, awayTeam.name)
-#    meetings_draws = get5meetingsdraws(homeTeam.name, awayTeam.name)
-#    second_team_5meetings_wins = get5meetingsloses(homeTeam.name, awayTeam.name)
-#else:    
-#    first_team_5meetings_wins = int(input("Enter " + homeTeam.name + " wins in last 5 meetings: "))
-#    meetings_draws = int(input("Enter number of draws last 5 meetings: "))
-#    second_team_5meetings_wins = int(input("Enter " + awayTeam.name + " wins in last 5 meetings: "))
-
-#if first_team_5meetings_wins > 5 :
-#    print("Error")
-#if meetings_draws > 5 :
-#    print("Error")
-#if second_team_5meetings_wins > 5 :
-#    print("Error")
-
-
-#meetings_percent_home = (first_team_5meetings_wins*100)/5
-#meetings_percent_draw = (meetings_draws*100)/5
-#meetings_percent_away = (second_team_5meetings_wins*100)/5
-
-#if debug == 1 :
-#    print("meetings:")
-#    print(str(meetings_percent_home))
-#    print(str(meetings_percent_draw))
-#    print(str(meetings_percent_away))
-
-#Last 3years home-away percents
-
-#if homeData == 1 and awayData ==1 :
-#    first_team_last3years_home_wins_percent = homeWins3years(homeTeam.name)
-#    first_team_last3years_home_draws_percent = homeDraws3years(homeTeam.name)
-#    first_team_last3years_home_loses_percent = homeLoses3years(homeTeam.name)
-#    second_team_last3years_away_wins_percent = awayWins3years(awayTeam.name)
-#    second_team_last3years_away_draws_percent = awayDraws3years(awayTeam.name) 
-#    second_team_last3years_away_loses_percent = awayLoses3years(awayTeam.name)
-#else :
-#    first_team_last3years_home_wins_percent = float(input("Enter last 3 years " + first_team_name + " home wins percent: "))
-#    first_team_last3years_home_draws_percent = float(input("Enter last 3 years " + first_team_name + " home draws percent: "))
-#    first_team_last3years_home_loses_percent = float(input("Enter last 3 years " + first_team_name + " home loses percent: "))
-
-#    second_team_last3years_away_wins_percent = float(input("Enter last 3 years " + second_team_name + " away wins percent: "))
-#    second_team_last3years_away_draws_percent = float(input("Enter last 3 years " + second_team_name + " away draws percent: "))
-#    second_team_last3years_away_loses_percent = float(input("Enter last 3 years " + second_team_name + " away loses percent: "))
-
-#home_away_percent1 = (first_team_last3years_home_wins_percent + second_team_last3years_away_loses_percent)/2
-#home_away_percentX = (first_team_last3years_home_draws_percent + second_team_last3years_away_draws_percent)/2
-#home_away_percent2 = (first_team_last3years_home_loses_percent + second_team_last3years_away_wins_percent)/2
-
-#if debug == 1 :
-#    print("last 3 years:")
-#    print(str(home_away_percent1))
-#    print(str(home_away_percentX))
-#    print(str(home_away_percent2))
-
+    
 #Calculated percents
 	
 calculated_home = (season_percent_home + last5games_percent_home)/2
@@ -224,7 +141,9 @@ with open("stats.txt", "a") as myfile:
     if bet_away + 10 < calculated_away :
         myfile.write("Bet 2 for " + str(bet2) + "\n")
         if calculated_away < 25 :
-            myfile.write("WARNING! bet 2 is under 25%\n") 
+            myfile.write("WARNING! bet 2 is under 25%\n")
+    myfile.write("RESULT:\n")
+    myfile.write("MONEY:\n")
     myfile.write("------------------\n") 
 myfile.close()
 
